05e5dd3864eb06495d5ea0d6e395b239c6ae415a
dyzpq <2580940614dyzpq>
2020-09-11 10:06:23 +0800

    并行流demo搭建

---
+++ java8/src/stream/StreamDemo3.java
@@ -0,0 +1,18 @@
+package stream;
+
+import org.junit.Test;
+
+/**
+ * @Author ddyy
+ * @Date 2020/9/10 0010 18:18
+ *
+ * 并行流：
+ *      并行流----->fork/join框架---->分治思想
+ */
+public class StreamDemo3 {
+
+    @Test
+    public void test1(){
+
+    }
+}
